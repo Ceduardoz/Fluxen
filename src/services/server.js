2e90d1065f56535b44eccc7f74c6f46669e2ea8a
ceduardoz <ceduardoz957@gmail.com>
2026-04-17 18:10:43 -0300

    feat: config api baseURL to use environment variables

--- src/services/server.js
+++ src/services/server.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://api-controle-financeiro-a1yr.onrender.com",
-  baseURL: "http://localhost:3333",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3333",
 });
 
 api.interceptors.request.use((config) => {
